e26154e0612d4e0a60fa9ca6e9c670a88ec48163
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-08 21:44:19 +0800

    Add alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    let startAlarm = 15
+    
     let eggTimes: [String : TimeInterval] = [
-        "Soft"      : 2 * 60,
+        "Soft"      : 5 * 60,
         "Medium"    : 7 * 60,
         "Hard"      : 12 * 60
     ]
     
     var deadline: Date?
+    var player: AVAudioPlayer?
     
     @IBOutlet weak var countdownLabel: UILabel!
     
@@ -40,8 +44,9 @@ class ViewController: UIViewController {
             countdownLabel.text = "\(remaining)"
             
             switch remaining {
-            case 1...60:
+            case 1...startAlarm:
                 countdownLabel.textColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+                soundAlarm()
             default:
                 countdownLabel.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
             }
@@ -49,12 +54,22 @@ class ViewController: UIViewController {
             if (d < Date()) {
                 deadline = nil
                 countdownLabel.text = "0"
+                player?.stop()
             }
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                 self.countDown()
             }
         }
+    }
+    
+    func soundAlarm() {
+        if (!(player?.isPlaying ?? false)) {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            self.player = try! AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.mp3.rawValue)
+            player?.play()
+        }
+        
     }
     
 }
